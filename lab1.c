048b52922cbd79cf7078ba58500b3e6d3156da9d
Carolosso <carolosso@outlook.com>
2022-10-19 22:03:58 +0200

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,21 @@
+#include <LCD.h>
+#include <reg515.sfr>
+
+int k[]={8,2,4,1};
+int i=0;
+
+void main()
+{
+	EAL=1;
+	ET2=1;
+	T2R1=1;
+	T2I0=1;
+}
+_interrupt 5 void n_f(void)
+{
+	P1=k[i];
+	i++;
+	if (i>3){
+	i=0;}
+	TF2=0;
+}
