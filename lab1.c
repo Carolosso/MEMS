7b0a37b170436e20d2ec5def3152b581e8e1e884
Carolosso <carolosso@outlook.com>
2022-10-20 10:18:26 +0200

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,23 +1,33 @@
-#include <LCD.h>
+#include <stdio.h>
 #include <reg515.sfr>
+#include <lcd.h>
 
-int k[]={8,2,4,1};
-int i=0;
-
-void main()
-{
+static int komutacja1_4[4]={8,2,4,1};
+static int i = 0;	static int j=0;
+void main(){
 	EAL=1;
 	ET2=1;
 	T2R1=1;
 	T2I0=1;
-}
-_interrupt 5 void n_f(void)
-{
-	P1=k[i];
-	i++;
-	if (i>3)
-	{
-		i=0;
+	while(1){
+		if(kb_test()=='4'){
+			if(CRC >= 20 && CRC <=2.5){
+				CRCH--;
+			}
+		}
+		if(kb_test()=='6'){
+		   if(CRC >= 20 && CRC <=2.5){
+				CRCH++;
+			}
+		}
+		for(j=0; j <2000; j++ ){}
 	}
+}
+_interrupt 5 void przerwanie(void){
+	if(i==4)i=0;
+	P1 = komutacja1_4[i];
+	i++;
 	TF2=0;
 }
+
+
