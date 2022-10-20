5dd4b97fe9586f32ac677674e197ba843c4e6558
Carolosso <carolosso@outlook.com>
2022-10-20 10:46:00 +0200

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -11,12 +11,12 @@ void main(){
 	T2I0=1;
 	while(1){
 		if(kb_test()=='4'){
-			if(CRC >= 20 && CRC <=2.5){
+			if(CRC >= 20){
 				CRCH--;
 			}
 		}
 		if(kb_test()=='6'){
-		   if(CRC >= 20 && CRC <=2.5){
+		   if(CRC <=2.5){
 				CRCH++;
 			}
 		}
