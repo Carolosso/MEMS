3eda3fb15ddf03240ca9142d4384b94e53929582
Carolosso <carolosso@outlook.com>
2022-12-01 10:59:32 +0100

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,29 +1,34 @@
-#include <LCD.h>
+#include <reg515.sfr>
+#include <lcd.h.>
+#include <mems.h>
 #include <regaduc812.sfr>
-void main()
-{
 unsigned int result;
-int a,b,c,d,i;
-l_start();
-ADCCON1=0x68;
-SCONV=1;
-while(1){
-  SCONV=1;
-  while(ADCCON3 & 0x80){
-    result = ADCDATAH << 8;
-    result |= ADCDATAL;
-    result=result*1.221;
-    a=result/1000;
-    b=(result-a*1000)/100;
-    c=(result-a*1000-b*100)/10;
-    d=result-a*1000-b*100-c*10;
-    l_cmd(1);
-    l_out(a+48);
-    l_out(44);
-    l_out(b+48);
-    l_out(c+48);
-    l_out(d+48);
-    for(i=5000000;i>0;i--);
-    }
- }
+static int res = 0;
+static int a,b,c,d=0;
+
+void main(){
+
+	ADCCON1=0x68;
+	init_lcd();
+	cmd_wr(1);
+
+	while(1){
+		SCONV=1;
+		while((ADCCON3 & 128) == 1){}  
+		result = ADCDATAH << 8;
+		result |= ADCDATAL;
+		res = result * 1.222;
+	   	
+		a = res/1000;
+		char_wr(a+48);
+		//wypisanie "."
+		char_wr(46);
+		b = (res - (a*1000))/100;
+		char_wr(b+48);
+		c = (res - (a*1000) - (b*100))/10;		   
+		char_wr(c+48);
+		d = res - (a*1000) - (b*100)-(c*10);		
+		char_wr(d+48);
+		cmd_wr(2);
+	}
 }
